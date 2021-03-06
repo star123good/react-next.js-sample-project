0ea303f89d30bd6c5c78e247e297ca532dbc3722
star123good <star921str@hotmail.com>
2021-03-06 23:35:42 +0800

    add package semantic-ui-react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 export default () => {
     return (
         <>
             <h1>Hello, there!</h1>
             <p>This is Index Page</p>
+            <Button>Ok!</Button>
         </>
     );
 };
